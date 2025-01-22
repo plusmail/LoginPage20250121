ef6ca071dfd26f76975180204cc0011610a111d6
plusmail <plus4957@gmail.com>
2025-01-22 16:41:37 +0900

    수정2

--- src/main/java/kroryi/loginpage/Application.java
+++ src/main/java/kroryi/loginpage/Application.java
@@ -12,10 +12,6 @@ public class Application extends javafx.application.Application {
         MyDB myDB = new MyDB(); // 단일 인스턴스 사용
 
         try{
-//            CommService commService = new CommonServiceImpl(stage);
-//            commService.showExamplePage("example.fxml");;
-//            commService.showLoginPage("login-view.fxml");
-//            commService.showListPage("list-view.fxml");
             SceneManager.setPrimaryStage(stage);
 //            SceneManager.switchScene("example.fxml","셈플");
             SceneManager.switchScene("login-view.fxml","셈플");
